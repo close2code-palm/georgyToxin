92828ec0e8430e3171ea9b306022e8138cb55b55
jusx_x <julyfortune101@gmail.com>
2021-03-20 21:21:21 +0300

    added colors

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = {
         {
           test: /\.svg$/,
           use: 'file-loader'
+        },
+        {
+            test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+            loader: 'file-loader?name=[name].[ext]'
         }
       ]
     },
